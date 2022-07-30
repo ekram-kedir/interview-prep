eff8129c37130f7f4a346a55dc3ec6f8a8209c96
Ekram Kedir <102755583+ekramkedir2020@users.noreply.github.com>
2022-07-30 10:02:23 +0300

    Create CountingValleys.py

---
+++ Solved_Questions/CountingValleys.py
@@ -0,0 +1,54 @@
+# An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly  steps, for every step it was noted if it was an uphill, , or a downhill,  step. Hikes always start and end at sea level, and each step up or down represents a  unit change in altitude. We define the following terms:
+
+# A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+# A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+# Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.
+
+# Example
+
+#  steps=8path=[DDUUUUDD]
+
+# The hiker first enters a valley  units deep. Then they climb out and up onto a mountain  units high. Finally, the hiker returns to sea level and ends the hike.
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    valley=0
+    altitude=0
+    for i in path:
+        if i=="U":
+            if altitude==-1:
+                valley+=1
+            altitude+=1
+            
+        else:
+            altitude-=1
+    return valley
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
