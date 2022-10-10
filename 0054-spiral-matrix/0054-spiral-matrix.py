24922c02668beccd9bcf53d58cbd890d2c60daf1
Ekram Kedir <102755583+ekramkedir2020@users.noreply.github.com>
2022-10-10 12:22:15 +0300

    Time: 62 ms (23.79%), Space: 14 MB (33.88%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,22 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix:
+            return []
+        ans=[]
+        start,end,left,right=0,len(matrix),0,len(matrix[0])
+        while start<end or left<right:
+            if start<end:
+                ans.extend([matrix[start][i] for i in range (left,right)])
+                start+=1
+            if left<right:
+                ans.extend([matrix[i][right-1] for i in range (start,end)])
+    
+                right-=1
+            if start<end:
+                ans.extend([matrix[end-1][i] for i in range (right-1,left-1,-1)])
+                end-=1
+            if left<right:
+                ans.extend([matrix[i][left] for i in range (end-1,start-1,-1)])
+    
+                left+=1
+        return ans
