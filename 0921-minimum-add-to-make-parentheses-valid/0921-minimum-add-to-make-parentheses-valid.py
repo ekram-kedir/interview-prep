7b18439a167de1e33a60a5bfe62ca25cd8d46b95
Ekram Kedir <102755583+ekramkedir2020@users.noreply.github.com>
2022-10-14 02:40:07 +0300

    Time: 76 ms (5.27%), Space: 13.8 MB (95.59%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -1,12 +1,12 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
         count=0
-        stack=[]
+        stack=0
         for i in s:
             if i=="(":
-                stack.append(i)
-            if stack and i==")":
-                stack.pop()
-            elif len(stack)==0 and  i==")":
+                stack+=1
+            if stack>0 and i==")":
+                stack-=1
+            elif stack==0 and  i==")":
                 count+=1
-        return len(stack)+count
+        return stack+count
