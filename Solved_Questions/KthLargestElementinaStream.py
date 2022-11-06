4f6f1c2153efb68b23ff0b6c81582a45100251cc
Ekram Kedir <102755583+ekramkedir2020@users.noreply.github.com>
2022-11-06 19:09:48 +0300

    Create KthLargestElementinaStream.py

---
+++ Solved_Questions/KthLargestElementinaStream.py
@@ -0,0 +1,35 @@
+# Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+# Implement KthLargest class:
+
+# KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+# int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
+
+# ANSWER
+
+class KthLargest(object):
+
+    def __init__(self, k, nums):
+        """
+        :type k: int
+        :type nums: List[int]
+        """
+        self.minHeap,self.k=nums,k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap)>k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val):
+        """
+        :type val: int
+        :rtype: int
+        """
+        heapq.heappush(self.minHeap,val)
+        if len(self.minHeap)>self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
