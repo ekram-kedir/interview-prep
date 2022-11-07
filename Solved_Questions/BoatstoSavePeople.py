d49dcc43b5829a508e2fc07691d212f69037b958
Ekram Kedir <102755583+ekramkedir2020@users.noreply.github.com>
2022-11-07 09:50:56 +0300

    Create BoatstoSavePeople.py

---
+++ Solved_Questions/BoatstoSavePeople.py
@@ -0,0 +1,31 @@
+# You are given an array people where people[i] is the weight of the ith person, and an infinite number of boats where each boat can carry a maximum weight of limit. Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
+
+# Return the minimum number of boats to carry every given person.
+
+ 
+
+# Example 1:
+
+# Input: people = [1,2], limit = 3
+# Output: 1
+# Explanation: 1 boat (1, 2)
+
+# ANSWER
+
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+        """
+        :type people: List[int]
+        :type limit: int
+        :rtype: int
+        """
+        ans=0
+        people.sort()
+        l,r=0,len(people)-1
+        while l<=r:
+            dif=limit-people[r]
+            r-=1
+            ans+=1
+            if dif>=people[l]:
+                l+=1
+        return ans
