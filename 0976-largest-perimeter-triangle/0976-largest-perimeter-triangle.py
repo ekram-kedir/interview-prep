2c56055a2a9ec813f63561b6235709c8bbd3d0e3
Ekram Kedir <102755583+ekramkedir2020@users.noreply.github.com>
2022-12-23 10:56:31 +0300

    Time: 438 ms (50.63%), Space: 15.5 MB (48.75%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
@@ -5,5 +5,5 @@ class Solution:
     
         for i in range(len(nums)-3,-1,-1):
             if nums[i] + nums[i + 1] > nums[i + 2]:
-                return nums[i] + nums[i + 1] + nums[i + 2]
+                return sum(nums[i:i + 3])
         return 0
