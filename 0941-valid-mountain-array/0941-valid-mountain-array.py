6923faa1eb735c3d5b161505a1cfacbf02a126fc
Ekram Kedir <102755583+ekramkedir2020@users.noreply.github.com>
2023-01-18 00:10:27 +0300

    Time: 200 ms (95.01%), Space: 15.5 MB (18.91%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.py
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -1,10 +1,23 @@
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
-        start , end , count = 0 , -1 ,len(arr)
+        max_value = max(arr)
+        index = arr.index(max_value)
+        if len(arr) > 2 and index != arr.index(arr[-1]) and index != arr.index(arr[0]): 
+            def before(arr,index):
+                before = arr[:index]
+                for i in range(len(before)-1):
+                    if before[i] >= before[i+1]:
+                        return False
+                return True
+            def after(arr,index):
+                after = arr[index:]
+                after = after[::-1]
+                for i in range(len(after)-1):
+                    if after[i] >= after[i+1]:
+                        return False
+                return True
+
+            return before(arr,index) and after(arr,index)
         
-        while start < count - 1 and arr[start] < arr[start + 1]:
-            start += 1
-        while end > - count and arr[end] < arr[end - 1]:
-            end -= 1
         
-        return start == end + count and 0 < start and end < -1
+                    
