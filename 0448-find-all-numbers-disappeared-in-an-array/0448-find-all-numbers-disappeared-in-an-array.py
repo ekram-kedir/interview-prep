72e7746a622974b30ec758bbbe5cadc097c00390
Ekram Kedir <102755583+ekramkedir2020@users.noreply.github.com>
2023-03-28 05:40:57 +0300

    Time: 446 ms (5.11%), Space: 22.3 MB (67.76%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findDisappearedNumbers(self, arr: List[int]) -> List[int]:
+        pointer = 0
+        length = len(arr)
+        answer = []
+        
+        while pointer < length:
+            reference = arr[pointer]
+            if (arr[pointer] != pointer + 1)  and arr[pointer] == arr[reference - 1]:
+                pointer += 1
+            elif arr[pointer] != pointer + 1:
+                arr[pointer] , arr[reference - 1] = arr[reference - 1] , arr[pointer]
+            else:
+                pointer += 1
+        for num in range(len(arr)):
+            if arr[num] != num + 1:
+                answer.append(num + 1)
+        return answer
+            
