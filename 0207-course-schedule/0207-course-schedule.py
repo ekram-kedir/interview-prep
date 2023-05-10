5df09c172161eb0842029314fed83f90e9f06c8b
Ekram Kedir <102755583+ekramkedir2020@users.noreply.github.com>
2023-05-10 19:28:52 +0300

    Time: 112 ms (24.66%), Space: 19.1 MB (11.97%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -7,14 +7,14 @@ class Solution:
         if node in callStack:
             return True
         
-        callStack.append(node)
+        callStack.add(node)
         
         for ngh in graph[node]:
             if self.dfs(ngh , graph , visited , callStack):
                 return True
             
         visited.add(node)
-        callStack.pop()
+        callStack.remove(node)
         return False
     
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
@@ -25,7 +25,7 @@ class Solution:
             graph[b].append(a)
             
         visited = set()
-        callStack = []
+        callStack = set()
         
         for a in range(numCourses):
             if self.dfs(a , graph , visited , callStack):
