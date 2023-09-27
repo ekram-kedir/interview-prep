92cc4dd29a65b99699a0facb4bd52e72e8da2a01
Ekram Kedir <102755583+ekram-kedir@users.noreply.github.com>
2023-09-27 05:29:26 +0300

    Time: 94 ms (58.19%), Space: 17.8 MB (85.99%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -4,17 +4,17 @@ class Solution:
         rows = len(grid)
         cols = len(grid[0])
         
-        dp = [[0 for col in range(cols)] for row in range(rows)]
+        dp = [[0 for _ in range(cols)] for _ in range(rows)]
         dp[0][0] = grid[0][0]
         
+        for col in range(1,cols):
+            dp[0][col] = grid[0][col] + dp[0][col - 1]
+            
         for row in range(rows):
             for col in range(cols):
-                
-                if row == 0 and col > 0:
-                    dp[row][col] = grid[row][col] + dp[row][col - 1]
-                elif col == 0 and row > 0:
-                    dp[row][col] = grid[row][col] + dp[row - 1][col]
+                if col == 0 and row > 0:
+                    dp[row][0] = grid[row][0] + dp[row - 1][0]
                 elif col > 0 and row > 0:
-                    dp[row][col] = min(dp[row][col - 1], dp[row - 1][col]) + grid[row][col]
-                
+                    dp[row][col] = min(dp[row - 1][col] , dp[row][col - 1]) + grid[row][col]
         return dp[-1][-1]
+        
