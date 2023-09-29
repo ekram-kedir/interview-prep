a6e2490bddef066d2efb3d0b173869759131ed00
Ekram Kedir <102755583+ekram-kedir@users.noreply.github.com>
2023-09-29 16:10:15 +0300

    Time: 598 ms (14.21%), Space: 189.2 MB (6.31%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,15 +1,18 @@
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
         
-        max_satisfaction = 0
         length = len(satisfaction)
-        
         satisfaction.sort()
-        
-        for ind in range(length):
-            sati = 0
-            for indx in range(ind , length):
-                sati += (indx + 1 - ind) * satisfaction[indx]
-            max_satisfaction = max(max_satisfaction , sati)
+        @cache
+        def helper(index , multiply):
             
-        return max_satisfaction
+            if index >= length:
+                return 0
+            
+            pick = helper(index + 1, multiply + 1) + (satisfaction[index] * multiply) 
+            dont_pick = helper(index + 1 , multiply)
+            
+            return max(pick , dont_pick)
+        
+        return helper(0 , 1)
+            
