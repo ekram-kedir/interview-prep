f872d0029144cc8d911eca0e8c4be0946fae9d3a
Ekram Kedir <102755583+ekram-kedir@users.noreply.github.com>
2023-09-29 15:32:04 +0300

    Time: 109 ms (36.98%), Space: 16.1 MB (96.73%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,16 +1,15 @@
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
         
-        satisfaction.sort(reverse=True)
+        max_satisfaction = 0
+        length = len(satisfaction)
         
-        total = 0
-        answer = 0
+        satisfaction.sort()
         
-        for satisfy in satisfaction:
+        for ind in range(length):
+            sati = 0
+            for indx in range(ind , length):
+                sati += (indx + 1 - ind) * satisfaction[indx]
+            max_satisfaction = max(max_satisfaction , sati)
             
-            total += satisfy
-            if total < 0:
-                break
-            answer += total
-            
-        return answer
+        return max_satisfaction
