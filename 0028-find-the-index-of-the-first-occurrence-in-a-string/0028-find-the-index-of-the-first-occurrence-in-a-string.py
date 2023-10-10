35f9884ba4579f418f19ae7b45e5f98fef7aff96
Ekram Kedir <102755583+ekram-kedir@users.noreply.github.com>
2023-10-10 15:19:36 +0300

    Time: 48 ms (5.95%), Space: 16.2 MB (51.80%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,10 +1,42 @@
 class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
+    def calculate(self , alphabets , length):
         
-        length = len(needle)
-
-        for i in range(len(haystack) - length + 1):
-            if haystack[i:i + length] == needle:
-                return i
+        offset = 96
+        result = 0
+        for index in range(len(alphabets)):
+            result += (ord(alphabets[index]) - offset) * (27**(length - index - 1))
+        return result
+        
+    def pollFirst(self, alphabet , result , length):
+        
+        offset = 96
+        return result - ((ord(alphabet) - offset) * (27**(length - 1)))
+      
+        
+    def addLast(self, currentAlphabet, result, length):
+                           
+        offset = 96 
+        return result * (27) + (ord(currentAlphabet) - offset)
+                           
+        
+    def strStr(self, haystack: str, needle: str) -> int:
+                           
+        length = len(needle)          
+        hash_code_needle = self.calculate(needle , length)
+        hash_code_haystack = self.calculate(haystack[:length] , length)
+        left = 0
+                                            
+        if hash_code_needle ==  hash_code_haystack:
+                return 0
             
+        for left in range(len(haystack) - length):
+                                            
+            hash_code_haystack = self.pollFirst(haystack[left] ,hash_code_haystack , length )
+            hash_code_haystack = self.addLast( haystack[left + length] ,hash_code_haystack , length)
+            
+            if hash_code_needle == hash_code_haystack:
+                    return left + 1
         return -1
+            
+
+                                 
