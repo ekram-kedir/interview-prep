51ec264b8cf386adc4f07f3bfbf2c83a1463c616
Ekram Kedir <102755583+ekram-kedir@users.noreply.github.com>
2023-11-27 02:34:47 +0300

    Time: 139 ms (85.18%), Space: 23.6 MB (27.31%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -1,6 +1,5 @@
 class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
-        
         @cache
         def collatz_count(num):
             if num == 1:
@@ -11,7 +10,5 @@ class Solution:
                 return 1 + collatz_count(3 * num + 1)
 
         elements = list(range(lo, hi + 1))
-        elements.sort(key=lambda x: (collatz_count(x), x))
+        elements.sort(key=collatz_count)
         return elements[k - 1]
-        
-                
